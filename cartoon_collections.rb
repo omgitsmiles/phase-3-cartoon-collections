8215cce1bf6a3c056f221e39f817773fab8d55f2
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-19 15:37:33 -0400

    commit tree 4d63a6d525d8ea382de5b8940053a86b08cc030b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+   dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{ |planeteers| planeteers.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.size > 4 ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find {|cheese| cheese_types.include?(cheese)}
 end
+
+
+# binding.pry
+# "pls"
